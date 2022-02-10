5ef583f501c5ddeb21cd1f085f682d042809fc4e
Tommaso <tommaso.sassoli@stud.unifi.it>
2022-02-10 17:11:27 +0100

    Report generator fixed

--- src/test/java/tfs/gui/report/ReportGeneratorTest.java
+++ src/test/java/tfs/gui/report/ReportGeneratorTest.java
@@ -1,5 +1,6 @@
 package tfs.gui.report;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tfs.business.model.customer.Customer;
 import tfs.business.model.estimate.Estimate;
@@ -13,15 +14,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReportGeneratorTest {
     ReportGenerator generator;
+    Estimate estimate;
+
+    @BeforeEach
+    void setUp() {
+        estimate = new Estimate("0", new Customer(), LocalDateTime.now());
+        ItemGroup g = new ItemGroup();
+        g.addItem(new Item());
+        estimate.addItemGroup(g);
+    }
 
     @Test
     void print() {
-        Estimate e = new Estimate("0", new Customer(), LocalDateTime.now());
-        ItemGroup g = new ItemGroup();
-        g.addItem(new Item());
-        e.addItemGroup(g);
+        generator = new ReportGenerator(estimate, FileResolver.REPORT_ESTIMATE);
+        generator.print();
 
-        generator = new ReportGenerator(e, FileResolver.REPORT_ESTIMATE);
+        assertTrue(generator.isPrintSuccessful());
+    }
+
+    @Test
+    void printB() {
+        generator = new ReportGenerator(estimate, FileResolver.REPORT_ESTIMATE_B);
         generator.print();
 
         assertTrue(generator.isPrintSuccessful());
